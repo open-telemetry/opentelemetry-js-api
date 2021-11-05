c69ac04bb19f4e31267e065ec1d81fa36a818f66
James Park <jamesjhp@amazon.com>
2021-11-05 12:28:28 -0700

    modify documentation

--- src/common/Time.ts
+++ src/common/Time.ts
@@ -22,7 +22,7 @@
  * The first number is calculated by converting and truncating the Epoch time in milliseconds to seconds:
  * HrTime[0] = Math.trunc(1609504210150 / 1000) = 1609504210.
  * The second number is calculated by converting the digits after the decimal point of the subtraction, (1609504210150 / 1000) - HrTime[0], to nanoseconds:
- * HrTime[1] = Number((1609504210.150 - HrTime[0]).toFixed(9)) * Math.pow(10, 9) = 150000000.
+ * HrTime[1] = Number((1609504210.150 - HrTime[0]).toFixed(9)) * 1e9 = 150000000.
  * This is represented in HrTime format as [1609504210, 150000000].
  */
 export type HrTime = [number, number];
